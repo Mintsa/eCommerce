88b172594c3477790a8048876d2c7e84d833b646
Romuald MBA <mbamintsaromuald@yahoo.fr>
2020-05-02 17:49:27 +0200

    Initial application generated by JHipster-6.2.0

---
+++ src/main/webapp/app/account/account.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ECommerceSharedModule } from 'app/shared';
+
+import {
+  PasswordStrengthBarComponent,
+  RegisterComponent,
+  ActivateComponent,
+  PasswordComponent,
+  PasswordResetInitComponent,
+  PasswordResetFinishComponent,
+  SettingsComponent,
+  accountState
+} from './';
+
+@NgModule({
+  imports: [ECommerceSharedModule, RouterModule.forChild(accountState)],
+  declarations: [
+    ActivateComponent,
+    RegisterComponent,
+    PasswordComponent,
+    PasswordStrengthBarComponent,
+    PasswordResetInitComponent,
+    PasswordResetFinishComponent,
+    SettingsComponent
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class ECommerceAccountModule {}
